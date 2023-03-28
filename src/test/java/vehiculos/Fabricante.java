7635802918a24f462f4b612490f39b75a1de3da5
shurming01 <salashiguita.rs@gmail.com>
2023-03-28 03:17:27 -0500

    Commit

--- src/test/java/vehiculos/Fabricante.java
+++ src/test/java/vehiculos/Fabricante.java
@@ -1,27 +1,50 @@
 package vehiculos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fabricante {
 	private String nombre; // nombre fabricante
 	private Pais pais;  // nombre ubicación fabricante
+	
+	List<String> fabricas = new ArrayList<String>();
+	List<Integer> cantidadf = new ArrayList<Integer>();
 	
 	
 	public Fabricante(String nombre, Pais pais) {
 		this.nombre = nombre;
 		this.pais = pais;
 	}
-
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public String fabricaMayorVentas() {
+		int indiceMayor = 0;
+		String busqueda = this.nombre;
+		int indice = fabricas.indexOf(busqueda);
+		
+		if(indice == -1) { 
+			fabricas.add(this.nombre);
+			int indice2 = fabricas.indexOf(busqueda);
+			cantidadf.add(indice2,+1);
+		}else {
+			int indice2 = fabricas.indexOf(busqueda);
+			cantidadf.add(indice2,+1);
+		}
+		
+		for(int x = 1; x < fabricas.size();x++){
+			
+			if (cantidadf.get(x) > cantidadf.get(indiceMayor)) {
+				
+				indiceMayor = x;
+			}
+		}
+		
+		int masVentas = cantidadf.get(indiceMayor);
+		String fabricaMayorVentas = fabricas.get(indiceMayor);
+		
+		return fabricaMayorVentas;
+		
+		
+	}
 	
 
 	public String getNombre() {
--- src/test/java/vehiculos/Pais.java
+++ src/test/java/vehiculos/Pais.java
@@ -6,16 +6,46 @@ import java.util.List;
 
 public class Pais {
 	private String nombre; // nombre del pais
+	//public static int cont = 0;
 	
 	List<String> pais = new ArrayList<String>();
-	List<Integer> Cantidad = new ArrayList<Integer>();
+	List<Integer> cantidad = new ArrayList<Integer>();
 	
 	
-	public static String paisMasVendedor() {
-		String nombrePais;
-		
-		nombrePais  = Fabricante.getPais();
-		
+	public Pais(String nombre) {
+		this.nombre = nombre;
 	}
+
+
+	public  String paisMasVendedor() {
+		int mayorIndice = 0;
+		String busqueda = this.nombre;
+		int indice = pais.indexOf(busqueda);
+		
+		if(indice == -1) { // Si es -1 es que la busqueda no esta
+			pais.add(this.nombre);
+			int indice2 = pais.indexOf(busqueda);
+			cantidad.add(indice2,+1);
+		}else {
+			int indice2 = pais.indexOf(busqueda);
+			cantidad.add(indice2,+1);
+		}
+		
+		for(int i = 1; i < pais.size();i++){
+			
+			if (cantidad.get(i) > cantidad.get(mayorIndice)) {
+				
+				mayorIndice = i;
+			}
+		}
+		
+		int maxCantidad = cantidad.get(mayorIndice);
+		String paisMasVendedor = pais.get(mayorIndice);
+		
+		return paisMasVendedor;
+
+	}
+	
+	
 	
 }
