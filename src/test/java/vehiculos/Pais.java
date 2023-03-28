52ae2ffa19d938fa150f13f6ca162c8452374570
shurming01 <salashiguita.rs@gmail.com>
2023-03-28 01:29:48 -0500

    Commit

--- src/test/java/vehiculos/Pais.java
+++ src/test/java/vehiculos/Pais.java
@@ -1,5 +1,21 @@
 package vehiculos;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Pais {
 	private String nombre; // nombre del pais
+	
+	List<String> pais = new ArrayList<String>();
+	List<Integer> Cantidad = new ArrayList<Integer>();
+	
+	
+	public static String paisMasVendedor() {
+		String nombrePais;
+		
+		nombrePais  = Fabricante.getPais();
+		
+	}
+	
 }
